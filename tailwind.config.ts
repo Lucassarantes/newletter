dcdfa6d9462c0d56e24f492a4c09f54e99d27dfc
Lucas Salles <lucas.sarantes@gmail.com>
2023-08-08 14:55:31 -0300

    Configuração inicial do projeto.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,28 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      //### Primary
+
+      //'Tomato'
+      'primary-tomato': 'hsl(4, 100%, 67%)',
+
+      //### Neutral
+      neutral: {
+        'Dark Slate Grey': 'hsl(234, 29%, 20%)',
+        'Charcoal Grey': 'hsl(235, 18%, 26%)',
+        'Grey': 'hsl(231, 7%, 60%)',
+        'White': 'hsl(0, 0%, 100%)',
+      }
+    },
     extend: {
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif']
+      },
+      fontWeight: {
+        'normal': '400',
+        'bold': '700'
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
